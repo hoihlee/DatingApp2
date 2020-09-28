c46c49eca00d91ab18f73368c3189f92ee8b042e
Hoi <hoi.h.lee@gmail.com>
2020-09-27 23:26:30 -0400

    EndofSection5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -46,38 +46,39 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
-
-            if (userFromRepo == null)
-                return Unauthorized();
-
-            // build token JWT
-            // 1. user id
-            // 2. user name
-            var claims = new[]{
-                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
-                new Claim(ClaimTypes.Name, userFromRepo.Username)
-            };
-
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
-
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha384Signature); 
             
-            var tokenDescriptor = new SecurityTokenDescriptor{
-                Subject = new ClaimsIdentity(claims),
-                Expires = System.DateTime.Now.AddDays(1),
-                SigningCredentials = creds
-            }; 
+                var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
-            var tokenHandler = new JwtSecurityTokenHandler(); 
+                if (userFromRepo == null)
+                    return Unauthorized();
 
-            var token = tokenHandler.CreateToken(tokenDescriptor); 
+                // build token JWT
+                // 1. user id
+                // 2. user name
+                var claims = new[]{
+                    new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                    new Claim(ClaimTypes.Name, userFromRepo.Username)
+                };
 
-            return Ok(new {
-                token = tokenHandler.WriteToken(token)
-            }); 
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
 
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha384Signature);
 
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(claims),
+                    Expires = System.DateTime.Now.AddDays(1),
+                    SigningCredentials = creds
+                };
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                return Ok(new
+                {
+                    token = tokenHandler.WriteToken(token)
+                });
         }
 
     }
